5aa98d903c08e15cbdbae7c46317ac8d18366d0b
Hamel Husain <hamel.husain@gmail.com>
2023-10-03 11:32:55 -0700

    change sweep params

--- deepspeed/sweep.sh
+++ deepspeed/sweep.sh
@@ -4,14 +4,14 @@
 set +e
 
 # Loop through each option
-for ds_stg in 0 1 2 3; do
+for ds_stg in 0 2 3; do
   for n_gpu in 1 2 3; do
     for gc in True False; do
       for seq_len in 64 256 512 1024 2048; do
         for model_sz in 7 13 34; do
           # Conditional batch sizes based on sequence length
           if [ $seq_len -le 256 ]; then
-            bs_list=(1 4 8 16 32 64 100 128 200 256)
+            bs_list=(1 4 8 16 32 64 100 200)
           else
             bs_list=(1 3 4 6 8 16 32)
           fi
