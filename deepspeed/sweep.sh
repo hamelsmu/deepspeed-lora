17793fa96e21d221c2c4399dba3521116131db35
Hamel Husain <hamel.husain@gmail.com>
2023-10-03 11:41:29 -0700

    narrow sweep

--- deepspeed/sweep.sh
+++ deepspeed/sweep.sh
@@ -11,9 +11,9 @@ for ds_stg in 0 2 3; do
         for model_sz in 7 13 34; do
           # Conditional batch sizes based on sequence length
           if [ $seq_len -le 256 ]; then
-            bs_list=(1 4 8 16 32 64 100 200)
+            bs_list=(1 8 64 200)
           else
-            bs_list=(1 3 4 6 8 16 32)
+            bs_list=(1 3 4 6 8 32)
           fi
 
           for bs in "${bs_list[@]}"; do
