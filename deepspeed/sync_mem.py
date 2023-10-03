e23ae6315416298407106ec365e9b0d1ae2b892a
Hamel Husain <hamel.husain@gmail.com>
2023-10-03 10:42:38 -0700

    handle syncing

--- deepspeed/sync_mem.py
+++ deepspeed/sync_mem.py
@@ -1,12 +1,18 @@
 from fastcore.utils import Path
 import wandb
-import json
+from wandb import CommError
+import json, os, time
 
 def read_and_sync_to_wandb(wandb_user, wandb_proj, run_id):
     dpth = Path(f"mem_data/{run_id}")
     api = wandb.Api()
+
     run_path = f"{wandb_user}/{wandb_proj}/{run_id}"
-    run = api.run(run_path)
+    try:
+        run = api.run(run_path)
+    except CommError:
+        print(f"Run {run_path} does not exist. Skipping...")
+        return
     for f in dpth.ls():
         if f.name.endswith(".json"):
             with open(f, 'r') as f:
@@ -16,8 +22,19 @@ def read_and_sync_to_wandb(wandb_user, wandb_proj, run_id):
                 run.summary[f"gpu_{gpu}_mem"] =  mem
     run.summary.update()
 
+if __name__ == '__main__':
+    try:
+        with open("last_run_timestamp.txt", "r") as f:
+            last_run_timestamp = float(f.read())
+    except FileNotFoundError:
+        last_run_timestamp = 0.0
+    
+    for d in Path('mem_data').ls(): 
+        if d.name.startswith('z') and d.is_dir():
+            if any(os.path.getmtime(f) > last_run_timestamp for f in d.ls()):
+                print(f"syncing {d.name} to wandb")
+                read_and_sync_to_wandb('hamelsmu', 'deepspeed-data', d.name)
 
-for d in Path('mem_data').ls(): 
-    if d.name.startswith('z') and d.is_dir():
-        print(f"syncing {d.name} to wandb")
-        read_and_sync_to_wandb('hamelsmu', 'deepspeed-data', d.name)
+    current_timestamp = time.time()
+    with open("last_run_timestamp.txt", "w") as f:
+        f.write(str(current_timestamp))
